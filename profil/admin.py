c195de34f8c169c566611da3c2094972d670170b
kanza <kanza190504@gmail.com>
2025-06-02 17:28:26 +0700

    Push to new repository

--- profil/admin.py
+++ profil/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import Barang
 # Register your models here.
+
+class BarangAdmin(admin.ModelAdmin):
+    list_display = ('kdbrg', 'nama', 'stok', 'harga', 'link_gbr')
+    list_filter = ('nama',)
+    search_fields = ('nama', 'harga')
+
+admin.site.register(Barang, BarangAdmin)
