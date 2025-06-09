e825d7c2de6caa47a3cebaef673a7b2b6f134268
kanza <kanza190504@gmail.com>
2025-06-09 17:15:16 +0700

    pert10

--- profil/admin.py
+++ profil/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from . models import Barang
+from . models import Barang,Jenis
 # Register your models here.
 
 class BarangAdmin(admin.ModelAdmin):
-    list_display = ('kdbrg', 'nama', 'stok', 'harga', 'link_gbr')
-    list_filter = ('nama',)
-    search_fields = ('nama', 'harga')
+    list_display = ('kdbrg', 'nama', 'stok', 'harga', 'link_gbr', 'jenis_id')
+    list_filter = ('jenis_id', )
+    search_fields = ('nama', 'kdbrg', 'jenis_id__nama')
+    list_per_page = 3
 
 admin.site.register(Barang, BarangAdmin)
+admin.site.register(Jenis)
